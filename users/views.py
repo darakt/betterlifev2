92ffab842a4757c1bd5f8a02ae86ddce0a6b02b8
darakt <kheriboum@gmail.com>
2022-04-29 18:18:23 +0200

    :construction: POST not allowed on create

--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from .models import User
+from .serializers import UserSerializer
+from rest_framework.response import Response
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def create(self, request):
+        print(request)
+        user = User.objects.create(request)
+        print(user.toJson())
+        return Response(user)
